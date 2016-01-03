15cee291beb2543b6839466365aa768e05531a77
agsgs2007 <zhangxun0327@126.com>
2016-01-03 22:41:25 +0800

    试验自定义的log

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,18 @@
+# -*- coding:utf-8 -*-
+
 from django.shortcuts import render
 
+#在此次创建日志器，根据设定输出错误信息
+import logging
+logger = logging.getLogger('blog.views')
+
+
 def index(request):
+
+    # 不存在的文件，输出错误日志
+    try:
+        file = open('sss.txt', 'r')
+    except Exception as e:
+        logger.error(e)
+
     return render(request, 'index.html', locals())
