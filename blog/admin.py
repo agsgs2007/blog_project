95dfee1caab886229d1b2d0ff2224ec28f77eec0
agsgs2007 <zhangxun0327@126.com>
2016-01-05 20:49:29 +0800

    注册admin信息，创建超级用户，登陆admin页面

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
+# -*- coding:utf-8 -*-
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+# 自定义管理页面显示的内容
+class AdAdmin(admin.ModelAdmin):
+
+    fields = ('title', 'description',)
+    list_display = ('title', 'description',)  #列表显示内容
+
+# 使用默认的方法在admin模块中注册模型，然后可以登录admin网页，需要
+# 提前manage.py createsuperuser创建超级用户
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Ad, AdAdmin)
+admin.site.register(Article)
+admin.site.register(Links)
+admin.site.register(Tag)
+admin.site.register(Comment)
